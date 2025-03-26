2a383c7ca59daf945ad20721e5f63e822d879011
rhgrive2 <hataritu70@gmail.com>
2025-03-26 14:04:30 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,22 +2,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "MixedLanguagePackage",
+    name: "ui",
     platforms: [.iOS(.v13)],
     products: [
         .library(
-            name: "MixedLanguagePackage",
-            targets: ["MixedLanguagePackage"]
+            name: "ui",
+            targets: ["ui"]
         ),
     ],
     targets: [
         .target(
-            name: "MixedLanguagePackage",
+            name: "ui",
             path: "Sources",
-            exclude: ["ObjC/test.h"], // 公開しないヘッダーは除外
-            publicHeadersPath: "ObjC", // 公開ヘッダーのディレクトリ
+            sources: ["Swift/test.swift", "ObjC/test.m"],  // 明示的にファイル指定
+            publicHeadersPath: "ObjC",  // ヘッダーファイルの場所
             cSettings: [
-                .headerSearchPath("ObjC"), // ヘッダー検索パス
+                .headerSearchPath("ObjC"),  // ヘッダー検索パス
+                .unsafeFlags(["-fmodules"])  // モジュールサポートを有効化（CIで必要になる場合あり）
             ]
         )
     ]
