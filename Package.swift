bf391d5eb68a3c5cf7992d9bb91202ed312a60d7
rhgrive2 <hataritu70@gmail.com>
2025-03-26 13:55:07 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,21 +5,18 @@ let package = Package(
     name: "SwiftObjcIntegration",
     platforms: [.iOS(.v13)],
     products: [
-        .library(name: "SwiftObjcIntegration", type: .dynamic, targets: ["SwiftObjcIntegrationSwift"]),
+        .library(name: "SwiftObjcIntegration", type: .dynamic, targets: ["SwiftObjcIntegration"]),
     ],
     targets: [
         .target(
-            name: "SwiftObjcIntegrationSwift",
+            name: "SwiftObjcIntegration", // 全体のターゲット名
             dependencies: [],
-            path: "Sources/Swift",
+            path: "Sources",
+            sources: ["Swift/test.swift", "ObjC/test.m"], // Swift と ObjC のファイルを指定
             cSettings: [
                 .headerSearchPath("Sources/ObjC")
             ]
         ),
-        .target(
-            name: "SwiftObjcIntegrationObjC",
-            dependencies: [],
-            path: "Sources/ObjC"
-        )
     ]
 )
+
