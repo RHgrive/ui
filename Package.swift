04838ed830ba0a0478e87ac3497702215772e30b
rhgrive2 <hataritu70@gmail.com>
2025-03-26 14:07:30 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.10
+// swift-tools-version: 5.10
 import PackageDescription
 
 let package = Package(
@@ -7,26 +7,23 @@ let package = Package(
     products: [
         .library(
             name: "ui",
+            type: .dynamic,  // 明示的に dynamic library を指定
             targets: ["ui"]
         ),
     ],
     targets: [
         .target(
-            name: "ObjCModule",
-            path: "Sources/ObjC",
-            publicHeadersPath: ".",
-            cSettings: [
-                .headerSearchPath(".")
-            ]
-        ),
-        .target(
-            name: "SwiftModule",
-            dependencies: ["ObjCModule"],
-            path: "Sources/Swift"
-        ),
-        .target(
             name: "ui",
-            dependencies: ["ObjCModule", "SwiftModule"]
+            path: "Sources",
+            sources: ["Swift/test.swift", "ObjC/test.m"],
+            publicHeadersPath: "ObjC",
+            cSettings: [
+                .headerSearchPath("ObjC"),
+                .unsafeFlags(["-fmodules"])
+            ],
+            linkerSettings: [
+                .unsafeFlags(["-Xlinker", "-objc"])
+            ]
         )
     ]
 )
